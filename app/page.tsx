2daa02ef14ea4375baed6a66f0340fbdab91359d
bharadwaj palakurthy <bharadwajpalakurthy@gmail.com>
2023-06-15 18:15:03 +0530

    implemented contracts

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,40 @@
 import FundsUI from "./fundComponent";
 import styles from "./page.module.css";
+import { ethers } from "ethers";
+import abi from "../app/config/faucetABI.json";
 
 export default function Home() {
   async function sendFunds(formData: any) {
     "use server";
-    console.log(process.env.PRIVATE_KEY);
-    console.log(formData);
+
+    //ethers provider using RPC URL
+    const provider = new ethers.JsonRpcProvider(process.env.RPC_URL);
+
+    if (!process.env.PRIVATE_KEY) {
+      throw new Error("PRIVATE_KEY is not defined");
+    }
+    //ethers signer using private key
+    const signer = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
+
+    const FaucetContract = new ethers.Contract(
+      "0xDFd5Fab8372F5E92CC09A6a519cbAb1F97438cad",
+      abi,
+      signer
+    );
+    console.log(formData.tokens.note);
+    try {
+      const tx = await FaucetContract.getNamesTokens(
+        formData.address,
+        ethers.parseUnits("10", 18),
+        formData.tokens.note,
+        formData.tokens.usdc,
+        formData.tokens.usdt,
+        formData.tokens.eth,
+        formData.tokens.atom
+      );
+    } catch (error: any) {
+      console.error(error.info.error.message);
+    }
   }
   return (
     <main className={styles.main}>
