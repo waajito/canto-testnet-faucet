2abb2d34eacf8be90b5030b6fee24065263aa92c
bharadwaj palakurthy <bharadwajpalakurthy@gmail.com>
2023-06-27 17:39:34 +0530

    added validation logic and design changes.

--- app/fundComponent.tsx
+++ app/fundComponent.tsx
@@ -4,9 +4,9 @@ import { Checkbox } from "./components/Checkbox/Checkbox";
 import styles from "./page.module.css";
 import Image from "next/image";
 import { PT_Mono } from "next/font/google";
-import React from "react";
+import React, { useEffect } from "react";
 import { sendFunds } from "./actions";
-import { ethers } from "ethers";
+import { ethers, isAddress } from "ethers";
 import { abi, tokens as tokensConfig } from "./config/tokens";
 const ptMono = PT_Mono({ weight: "400", subsets: ["latin"] });
 
@@ -22,6 +22,10 @@ export default function FundsUI() {
     canto: false,
   });
 
+  const [balanceStatus, setBalanceStatus] = React.useState<
+    "NONE" | "LOADING" | "SUCCESS" | "ERROR"
+  >("NONE");
+
   const [balTokens, setBalTokens] = React.useState<Record<string, string>>({
     atom: "0",
     eth: "0",
@@ -30,12 +34,12 @@ export default function FundsUI() {
     note: "0",
     canto: "0",
   });
-
+  const [onSuccess, setOnSuccess] = React.useState(false);
   const [loading, setLoading] = React.useState(false);
   const [message, setMessage] = React.useState("Request Funds");
 
   //connect wallet
-  const getBalances = async () => {
+  async function getBalances() {
     //connecting to testnet
     const provider = new ethers.JsonRpcProvider(
       "https://canto-testnet.plexnode.wtf"
@@ -64,15 +68,20 @@ export default function FundsUI() {
         const balanceReducer = balances.reduce((prev, curr) => {
           return { ...prev, ...curr };
         }, {});
-
+        balanceReducer["canto"] = ethers.formatUnits(
+          await provider.getBalance(address),
+          18
+        );
         //set balances
         setBalTokens(balanceReducer);
+        setBalanceStatus("SUCCESS");
       });
     } else {
       setIsValidAddress(ethers.isAddress(address));
     }
-  };
+  }
 
+  //add canto test network
   async function addTestNetwork() {
     if (window.ethereum) {
       try {
@@ -133,205 +142,304 @@ export default function FundsUI() {
       }
     }
   }
+  const [totalSelectedTokens, setTotalSelectedTokens] = React.useState(0);
+  //check if any token is selected
+  useEffect(() => {
+    if (balanceStatus === "SUCCESS") {
+      //check if any token is selected
+      setTotalSelectedTokens(
+        Object.values(tokens).filter((token) => token === true).length
+      );
+
+      console.log(totalSelectedTokens);
+      //if no token is selected prevent default
+      if (Object.values(tokens).filter((token) => token === true).length == 0) {
+        setMessage("Select atleast one token");
+      } else {
+        setMessage("Request Funds");
+      }
+    }
+  }, [tokens, balanceStatus, totalSelectedTokens]);
+
+  //format token balance to currency
+  function formatCurrency(value: string): string {
+    if (Number(value) < 1 && Number(value) > 0) {
+      return Number(value).toFixed(4);
+    }
+    if (Number(value) >= 1 && Number(value) < 10000) {
+      return Number(value).toFixed(2);
+    }
+    if (Number(value) >= 10000 && Number(value) < 1000000) {
+      return (Number(value) / 1000).toFixed(2) + "k";
+    }
+    if (Number(value) >= 1000000) {
+      return (Number(value) / 1000000).toFixed(2) + "m";
+    }
+    return value;
+  }
+
   return (
     <div className={styles.card}>
       <h2 className="title">Canto Testnet Faucet</h2>
       <Image src="/logo.svg" alt="canto" width={100} height={60} />
 
-      <div className="row">
-        <button
-          className={styles.button}
-          style={{
-            minWidth: "10rem",
-          }}
-          onClick={addTokens}
-        >
-          import test tokens
-        </button>
-        <button
-          className={styles.button}
-          style={{
-            minWidth: "10rem",
-          }}
-          onClick={addTestNetwork}
-        >
-          add network
-        </button>
-        <button
-          className={styles.button}
-          style={{
-            minWidth: "10rem",
-          }}
-          onClick={getBalances}
-        >
-          get balances
-        </button>
-      </div>
-      <form
-        action={() => {
-          sendFunds({ address, tokens }).then((res) => {
-            setLoading(false);
-            if (res.status === 200) {
-              setMessage("Funds Sent");
-            } else {
-              setMessage("Something went wrong");
-            }
-          });
-        }}
-      >
-        <input
-          className={styles.input}
-          type="text"
-          name="address"
-          id="address"
-          placeholder="Enter The Address"
-          autoComplete="off"
-          value={address}
-          required
-          onChange={(e) => setAddress(e.target.value)}
-        />
-        <div className={styles["tokens-grid"]}>
-          <div className={styles.item}>
-            <div className="token-name">ATOM</div>
-
-            <Checkbox
-              label="ATOM"
-              onChange={() => {
-                setTokens({
-                  ...tokens,
-                  atom: !tokens.atom,
-                });
-              }}
-            />
-          </div>
-          <div className={styles.item}>
-            <div className="token-name">ETH</div>
-
-            <Checkbox
-              label="ETH"
-              onChange={() => {
-                setTokens({
-                  ...tokens,
-                  eth: !tokens.eth,
-                });
-              }}
-            />
-          </div>
-
-          <div className={styles.item}>
-            <div className="token-name">USDC</div>
-
-            <Checkbox
-              label="USDC"
-              onChange={() => {
-                setTokens({
-                  ...tokens,
-                  usdc: !tokens.usdc,
-                });
-              }}
-            />
-          </div>
-        </div>
-
-        <div className={styles["tokens-grid"]}>
-          <div className={styles.item}>
-            <div className="token-name">USDT</div>
-
-            <Checkbox
-              label="USDT"
-              onChange={() => {
-                setTokens({
-                  ...tokens,
-                  usdt: !tokens.usdt,
-                });
-              }}
-            />
-          </div>
-          <div className={styles.item}>
-            <div className="token-name">Canto</div>
-
-            <Checkbox
-              label="Canto"
-              onChange={() => {
-                setTokens({
-                  ...tokens,
-                  canto: !tokens.canto,
-                });
-              }}
-            />
-          </div>
-          <div className={styles.item}>
-            <div className="token-name">NOTE</div>
-
-            <Checkbox
-              label="NOTE"
-              onChange={() => {
-                setTokens({
-                  ...tokens,
-                  note: !tokens.note,
-                });
-              }}
-            />
-          </div>
-        </div>
-        <div className={styles["balance-container"]}>
-          <div className={styles["balance-grid"]}>
-            <div className={styles.item}>
-              <div>Atom : </div>
-              <span className={styles["item-balance"]}>
-                {balTokens["atom"]}
-              </span>
-            </div>
-            <div className={styles.item}>
-              <div>ETH : </div>
-              <span className={styles["item-balance"]}>{balTokens.eth}</span>
-            </div>
-            <div className={styles.item}>
-              <div>USDC : </div>
-              <span className={styles["item-balance"]}>{balTokens.usdc}</span>
+      {onSuccess ? (
+        <>
+          <div className={styles["balance-container"]}>
+            <div className={styles["balance-grid"]}>
+              <div className={styles.item}>
+                <div>Atom : </div>
+                <span className={styles["item-balance"]}>
+                  {balTokens["atom"]}
+                </span>
+              </div>
+              <div className={styles.item}>
+                <div>ETH : </div>
+                <span className={styles["item-balance"]}>
+                  {formatCurrency(balTokens.eth)}
+                </span>
+              </div>
+              <div className={styles.item}>
+                <div>USDC : </div>
+                <span className={styles["item-balance"]}>
+                  {formatCurrency(balTokens.usdc)}
+                </span>
+              </div>
+              <div className={styles.item}>
+                <div>USDT : </div>
+                <span className={styles["item-balance"]}>
+                  {formatCurrency(balTokens.usdt)}
+                </span>
+              </div>
+              <div className={styles.item}>
+                <div>Canto : </div>
+                <span className={styles["item-balance"]}>
+                  {formatCurrency(balTokens.canto)}
+                </span>
+              </div>
+              <div className={styles.item}>
+                <div>Note : </div>
+                <span className={styles["item-balance"]}>
+                  {formatCurrency(balTokens.note)}
+                </span>
+              </div>
             </div>
           </div>
-          <div className={styles["balance-grid"]}>
-            <div className={styles.item}>
-              <div>USDT : </div>
-              <span className={styles["item-balance"]}>{balTokens.usdt}</span>
-            </div>
-            <div className={styles.item}>
-              <div>Canto : </div>
-              <span className={styles["item-balance"]}>{balTokens.canto}</span>
-            </div>
-            <div className={styles.item}>
-              <div>Note : </div>
-              <span className={styles["item-balance"]}>{balTokens.note}</span>
-            </div>
+          <p>
+            Thanks for using our test. Please return the funds once you are done
+            testing.
+          </p>
+        </>
+      ) : (
+        <>
+          <div className={styles.row}>
+            <button className={styles.button} onClick={addTokens}>
+              import tokens
+            </button>
+            <button className={styles.button} onClick={addTestNetwork}>
+              add network
+            </button>
           </div>
-        </div>
+          <form
+            action={() => {
+              console.log("sending funds");
+              sendFunds({ address, tokens }).then((res) => {
+                setLoading(false);
+                if (res.status === 200) {
+                  setOnSuccess(true);
+                  setMessage("Funds Sent");
+                } else {
+                  setMessage("Something went wrong");
+                }
+              });
+            }}
+          >
+            <input
+              className={styles.input}
+              type="text"
+              name="address"
+              id="address"
+              placeholder="Enter The Address"
+              autoComplete="off"
+              value={address}
+              required
+              onChange={(e) => setAddress(e.target.value)}
+            />
+            <div className={styles["tokens-grid"]}>
+              <div className={styles.item}>
+                <div className="token-name">ATOM</div>
 
-        <input
-          className={styles.button}
-          type="submit"
-          value={loading ? "Sending..." : message}
-          disabled={address.length < 15}
-          onClick={(e) => {
-            // e.preventDefault();
-            console.log("submitting");
+                <Checkbox
+                  label="ATOM"
+                  onChange={() => {
+                    setTokens({
+                      ...tokens,
+                      atom: !tokens.atom,
+                    });
+                  }}
+                />
+              </div>
+              <div className={styles.item}>
+                <div className="token-name">ETH</div>
 
-            setLoading(true);
-            // props.onSubmit({ address, tokens });
-          }}
-        />
-      </form>
+                <Checkbox
+                  label="ETH"
+                  onChange={() => {
+                    setTokens({
+                      ...tokens,
+                      eth: !tokens.eth,
+                    });
+                  }}
+                />
+              </div>
 
-      <p className={styles.description}>
-        if you’d like to return the funds please send them over to{" "}
-        <span
-          style={{
-            color: "#06fc99",
-          }}
-        >
-          0x4A2812DAf2432EAD79D2c20Ba84d65CbfA524A4D
-        </span>
-      </p>
+              <div className={styles.item}>
+                <div className="token-name">USDC</div>
+
+                <Checkbox
+                  label="USDC"
+                  onChange={() => {
+                    setTokens({
+                      ...tokens,
+                      usdc: !tokens.usdc,
+                    });
+                  }}
+                />
+              </div>
+            </div>
+
+            <div className={styles["tokens-grid"]}>
+              <div className={styles.item}>
+                <div className="token-name">USDT</div>
+
+                <Checkbox
+                  label="USDT"
+                  onChange={() => {
+                    setTokens({
+                      ...tokens,
+                      usdt: !tokens.usdt,
+                    });
+                  }}
+                />
+              </div>
+              <div className={styles.item}>
+                <div className="token-name">Canto</div>
+
+                <Checkbox
+                  label="Canto"
+                  onChange={() => {
+                    setTokens({
+                      ...tokens,
+                      canto: !tokens.canto,
+                    });
+                  }}
+                />
+              </div>
+              <div className={styles.item}>
+                <div className="token-name">NOTE</div>
+
+                <Checkbox
+                  label="NOTE"
+                  onChange={() => {
+                    setTokens({
+                      ...tokens,
+                      note: !tokens.note,
+                    });
+                  }}
+                />
+              </div>
+            </div>
+            <div className={styles["balance-container"]}>
+              <div className={styles["balance-grid"]}>
+                <div className={styles.item}>
+                  <div>Atom : </div>
+                  <span className={styles["item-balance"]}>
+                    {balTokens["atom"]}
+                  </span>
+                </div>
+                <div className={styles.item}>
+                  <div>ETH : </div>
+                  <span className={styles["item-balance"]}>
+                    {formatCurrency(balTokens.eth)}
+                  </span>
+                </div>
+                <div className={styles.item}>
+                  <div>USDC : </div>
+                  <span className={styles["item-balance"]}>
+                    {formatCurrency(balTokens.usdc)}
+                  </span>
+                </div>
+                <div className={styles.item}>
+                  <div>USDT : </div>
+                  <span className={styles["item-balance"]}>
+                    {formatCurrency(balTokens.usdt)}
+                  </span>
+                </div>
+                <div className={styles.item}>
+                  <div>Canto : </div>
+                  <span className={styles["item-balance"]}>
+                    {formatCurrency(balTokens.canto)}
+                  </span>
+                </div>
+                <div className={styles.item}>
+                  <div>Note : </div>
+                  <span className={styles["item-balance"]}>
+                    {formatCurrency(balTokens.note)}
+                  </span>
+                </div>
+              </div>
+            </div>
+
+            <input
+              className={styles.button}
+              style={{
+                minWidth: "100%",
+              }}
+              type="submit"
+              value={
+                !ethers.isAddress(address)
+                  ? "Enter Address"
+                  : balanceStatus === "NONE"
+                  ? "Get Balances"
+                  : balanceStatus === "LOADING"
+                  ? "fetching..."
+                  : loading
+                  ? "Sending..."
+                  : message
+              }
+              disabled={
+                !ethers.isAddress(address) ||
+                balanceStatus === "LOADING" ||
+                (balanceStatus === "SUCCESS" && totalSelectedTokens <= 0)
+              }
+              onClick={(e) => {
+                if (balanceStatus !== "SUCCESS") {
+                  e.preventDefault();
+                  setBalanceStatus("LOADING");
+                  getBalances();
+                } else {
+                  //if no token is selected prevent default
+                  if (totalSelectedTokens <= 0) {
+                    e.preventDefault();
+                  }
+                  setLoading(true);
+                }
+              }}
+            />
+          </form>
+
+          <p className={styles.description}>
+            if you’d like to return the funds please send them over to{" "}
+            <span
+              style={{
+                color: "#06fc99",
+              }}
+            >
+              0x4A2812DAf2432EAD79D2c20Ba84d65CbfA524A4D
+            </span>
+          </p>
+        </>
+      )}
     </div>
   );
 }
